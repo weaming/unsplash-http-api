0d676c239c3d3628d75e8e658ef82029e246f6bd
weaming <garden.yuen@gmail.com>
2018-08-26 14:20:48 +0800

    remove keys startswith _

--- api/to_json.py
+++ api/to_json.py
@@ -4,6 +4,9 @@ def obj_to_dict(obj):
     elif isinstance(obj, list):
         return [obj_to_dict(x) for x in obj]
     elif isinstance(obj, dict):
-        return {k: obj_to_dict(v) for k, v in obj.items()}
+        return {k: obj_to_dict(v) for k, v in obj.items() if str(k)[0] != '_'}
     else:
-        return {k: obj_to_dict(v) for k, v in obj.__dict__.items()}
+        return {
+            k: obj_to_dict(v)
+            for k, v in obj.__dict__.items() if str(k)[0] != '_'
+        }
